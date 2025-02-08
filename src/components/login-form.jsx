8fcaa0c2a13bf5ec6122c648a089e9c091e70184
Keishaaurel <keishaaureliarifai@gmail.com>
2025-02-08 14:07:42 +0700

    feat: create login page

--- src/components/login-form.jsx
+++ src/components/login-form.jsx
@@ -28,7 +28,7 @@ export function LoginForm({
             <div className="flex flex-col gap-6">
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+                <Input id="email" type="email" placeholder="caca@gmail.com" required />
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
